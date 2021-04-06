f6c2dc3bc1f782579414d83d149bc93e637164c9
Sungwoo Park <codest99@gmail.com>
2021-04-06 17:55:59 +0900

    Add random Python script

---
+++ member_random.py
@@ -0,0 +1,19 @@
+import random
+
+member = ['성우', '소현', '희제', '호찬']
+today = ['PS', 'OS/NET', 'JS', 'React']
+
+random_q = []
+
+while True:
+	random_q = [i for i in range(4)]
+	random.shuffle(random_q)
+	is_dup = False
+	for i in range(4):
+		if random_q[i] == i:
+			is_dup = True
+	if not is_dup:
+		break
+
+for idx, ran_num in enumerate(random_q):
+	print(f"{member[idx]}는 {today[ran_num]} 질문을 받는다!")
